721b9585c74fe62904b2fd66547b619a97d2fac1
Vasia15 <vasiampiztouna15@gmail.com>
2023-04-25 22:53:50 +0300

    Initial commit

--- main.tf
+++ main.tf
@@ -71,16 +71,10 @@ resource "azurerm_network_interface" "main" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.main.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.main1.id
+    public_ip_address_id          = azurerm_public_ip.main.id
   }
 }
-resource "azurerm_public_ip" "main1" {
-  name                = "${var.prefix}-codehub-publicip"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  allocation_method   = "Dynamic"
-  sku                 = "Standard"
-}
+
 resource "azurerm_virtual_machine" "main" {
   name                  = "${var.prefix}-codehub-vm"
   location              = azurerm_resource_group.main.location
